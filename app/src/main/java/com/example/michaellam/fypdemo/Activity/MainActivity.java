378b0521fb1efbeeca54f49a7b020f1317007ea6
Michael Lam <cfmlam@connect.ust.hk>
2017-01-05 22:47:14 +0800

    Http Request Done

---
+++ app/src/main/java/com/example/michaellam/fypdemo/Activity/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.michaellam.fypdemo.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.michaellam.fypdemo.R;
+
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    String token = "";
+    String userID = "";
+    String scope = "";
+    String tokenType = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+
+        final WebView login = (WebView) findViewById(R.id.Oauth);
+        String loginURL = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22874V&redirect_uri=http%3A%2F%2Fwww.ust.hk%2Fcallback.php&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
+        login.loadUrl(loginURL);
+
+        Button check = (Button) findViewById(R.id.tryButton);
+        final TextView status = (TextView) findViewById(R.id.StatusText);
+
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String[] inputS = {"https://api.fitbit.com/1/user/"+ userID +"/profile.json", tokenType + " " + token};
+                new httpGet().execute(inputS);
+            }
+        });
+
+        login.setWebViewClient(new WebViewClient()
+        {
+            public void onPageFinished(WebView view, String url) {
+                //System.out.println(login.getUrl());
+
+                String[] resultURL = login.getUrl().split("#access_token=");
+                resultURL = resultURL[resultURL.length - 1].split("&user_id=");
+                token = resultURL[0];
+                resultURL = resultURL[resultURL.length - 1].split("&scope=");
+                userID = resultURL[0];
+                resultURL = resultURL[resultURL.length - 1].split("&token_type=");
+                scope = resultURL[0];
+                resultURL = resultURL[resultURL.length - 1].split("&expires_in=");
+                tokenType = resultURL[0];
+                System.out.println(token);
+                System.out.println(userID);
+                System.out.println(scope);
+                System.out.println(tokenType);
+                System.out.println("Authentication Complete");
+                if (!login.getUrl().contains("fitbit")) {
+                    login.setVisibility(View.GONE);
+                    status.setText("Status: Success\n" + token);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/michaellam/fypdemo/Activity/httpGet.java
@@ -0,0 +1,82 @@
+package com.example.michaellam.fypdemo.Activity;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by MichaelLam on 5/1/2017.
+ */
+
+public class httpGet extends AsyncTask<String , Void ,String> {
+    String server_response;
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+        URL url;
+        HttpsURLConnection urlConnection = null;
+
+        try {
+            url = new URL(strings[0]);
+            urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Authorization", strings[1]);
+
+            int responseCode = urlConnection.getResponseCode();
+
+            if(responseCode == HttpsURLConnection.HTTP_OK){
+                server_response = readStream(urlConnection.getInputStream());
+                Log.v("CatalogClient", server_response);
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        //Log.e("Response", "" + server_response);
+
+
+    }
+
+// Converting InputStream to String
+
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response.toString();
+    }
+}
